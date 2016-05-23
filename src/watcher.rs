30030c3356eeeccc455fe869bb166fa26f172ddb
Keith Yeung <kungfukeith11@gmail.com>
2016-05-23 10:57:57 -0400

    Added getter, setter and register_callback

--- src/watcher.rs
+++ src/watcher.rs
@@ -2,7 +2,7 @@ use std::collections::HashSet;
 
 struct Watcher<T> {
     value: T,
-    callbacks: HashSet<fn(T)>
+    callbacks: HashSet<fn()>
 }
 
 impl<T> Watcher<T> {
@@ -10,6 +10,21 @@ impl<T> Watcher<T> {
         Watcher {
             value: value,
             callbacks: HashSet::new()
+        }
+    }
+
+    pub fn register_callback(&mut self, f: fn()) -> bool {
+        self.callbacks.insert(f)
+    }
+
+    pub fn get(&self) -> &T {
+        &self.value
+    }
+
+    pub fn set(&mut self, value: T) {
+        self.value = value;
+        for callback in &self.callbacks {
+            callback();
         }
     }
 }
