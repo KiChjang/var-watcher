c47081231d55af37d3b699468da4fe623dd8f87c
Keith Yeung <kungfukeith11@gmail.com>
2016-05-23 09:33:02 -0400

    Add fn new

--- src/watcher.rs
+++ src/watcher.rs
@@ -1,6 +1,15 @@
 use std::collections::HashSet;
 
 struct Watcher<T> {
-    inner: T,
+    value: T,
     callbacks: HashSet<fn(T)>
+}
+
+impl<T> Watcher<T> {
+    pub fn new(value: T) -> Watcher<T> {
+        Watcher {
+            value: value,
+            callbacks: HashSet::new()
+        }
+    }
 }
